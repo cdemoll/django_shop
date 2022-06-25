d048924ea7588bf9cfe15b17c7c7d2fbf735e405
Clément Demolliere <clemdem61@gmail.com>
2022-06-25 23:35:16 +0200

    Added mechanics for user logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth import get_user_model, login, logout
 
 User = get_user_model()
 
@@ -11,5 +11,9 @@ def signup(request):
         user = User.objects.create_user(username=username, password=password)
         login(request, user)
         return redirect('index')
-        
+
     return render(request, 'accounts/signup.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
